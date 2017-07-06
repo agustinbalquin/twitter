5d276260e3828b6155fa1a81ca1f90d63253fea1
agustinbalquin <agustinbalquin@gmail.com>
2017-07-06 16:59:04 -0700

    Added character count in post

--- twitter_alamofire_demo/ComposeController.swift
+++ twitter_alamofire_demo/ComposeController.swift
@@ -13,15 +13,18 @@ protocol ComposeControllerDelegate: NSObjectProtocol {
     func did(post: Tweet)
 }
 
-class ComposeController: UIViewController {
+class ComposeController: UIViewController, UITextViewDelegate {
 
-    @IBOutlet weak var tweetTextView: UITextView!
+
     
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var characterCountLabel: UILabel!
     
     weak var delegate: ComposeControllerDelegate!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        textView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,13 +32,20 @@ class ComposeController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+ 
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let newText = (textView.text as NSString).replacingCharacters(in: range, with: text)
+        let numberOfChars = newText.characters.count
+        var chars = 140 - numberOfChars
+        characterCountLabel.text =  "\(chars)"
+        return numberOfChars < 140
+    }
 
     @IBAction func cancelCompose(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     @IBAction func tweetMessage(_ sender: Any) {
-        let twit = tweetTextView.text
+        let twit = textView.text
         print(twit!)
         APIManager.shared.composeTweet(with: (twit!)) { (tweet, error) in
             if let error = error {
